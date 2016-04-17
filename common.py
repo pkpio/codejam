d1fc1d72fbfbad7683c9894194c0d342722ca1da
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-04-18 00:43:54 +0200

    Common template at root

---
+++ common.py
@@ -0,0 +1,49 @@
+#############   CONFIG  ##########
+prob = 'A'
+small = 0
+
+test = 0
+practice = 1
+attempt = 0
+##################################
+
+src = 'data/'
+dst = 'data/'
+if test:
+    src += 'test.in'
+    dst += 'test.out'
+else:
+    set = 'small' if small else 'large'
+    base = '{}-{}'.format(prob, set)
+    if practice:
+        base += '-practice'
+    else:
+        if small:
+            base += '-attempt' + str(attempt)
+    src += base + '.in'
+    dst += base + '.out'
+
+IN = open(src, 'r')
+OUT = open(dst, 'w')
+
+def readLine():
+    return IN.readline()
+
+def readInt():
+    return int(readLine())
+
+def readIntArr():
+    return [int(i) for i in readLine().split(' ')]
+
+def readCharArr():
+    return [i for i in readLine().split(' ')]
+
+def write(data):
+    OUT.write(data)
+
+def writeLine(data):
+    write(data + "\n")
+
+def done():
+    IN.close()
+    OUT.close()
