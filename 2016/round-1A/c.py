884ccaef89943310e230586bcc3f7604c1034ff6
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-04-17 23:03:00 +0200

    Cleanup in c sol

--- 2016/round-1A/c.py
+++ 2016/round-1A/c.py
@@ -4,7 +4,6 @@ from common import *
 #setInOut('B-large-practice.in','B-large-practice.out')
 
 import networkx as nx
-import matplotlib.pyplot as plt
 
 def maxInPathLen(DG, root):
     pres = DG.predecessors(root)
@@ -15,11 +14,7 @@ def maxInPathLen(DG, root):
 def getTwigLen(DG, cyc):
     DG.remove_edge(cyc[0], cyc[1])
     DG.remove_edge(cyc[1], cyc[0])
-    nx.draw(DG)
-    plt.show()
     return maxInPathLen(DG, cyc[0]) + maxInPathLen(DG, cyc[1])
-
-
 
 T = readInt()
 for t in range(T):
@@ -28,20 +23,10 @@ for t in range(T):
 
     DG=nx.DiGraph()
     DG.add_edges_from([(a,BFF[a]-1) for a in range(N)])
-
-    nx.draw(DG)
-    plt.show()
-
     cycles = list(nx.simple_cycles(DG))
     mx = 0
     for cyc in cycles:
         l = len(cyc)
-        #print(l)
-        if l == 2:
-            l += getTwigLen(DG.copy(), cyc)
-            #print(l)
+        l = l + getTwigLen(DG.copy(), cyc) if l == 2 else l
         mx = l if l>mx else mx
-
     print(mx)
-            #print(nx.eccentricity(DG, cyc[0]))
-            #print(nx.eccentricity(DG, cyc[1]))
