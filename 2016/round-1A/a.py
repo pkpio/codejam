83367c0c1d667e2d52b567a72ce6511af919e242
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-04-17 20:36:47 +0200

    Sol A updated

--- 2016/round-1A/a.py
+++ 2016/round-1A/a.py
@@ -1,12 +1,14 @@
-L = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+from common import *
+#setInOut('A-small-practice.in','A-small-practice.out')
+setInOut('A-large-practice.in','A-large-practice.out')
 
-T = int(input())
+####======= End of common template ===============####
+
+L = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+T = readInt()
 for t in range(T):
-    S = raw_input()
+    S = readLine()
     lword = S[0] + ''
     for c in S[1:]:
-        if L.index(lword[0]) <= L.index(c):
-            lword = c + lword
-        else:
-            lword = lword + c
-    print('Case #{}: {}'.format(t+1,lword))
+        lword = max(lword + c, c + lword)
+    write('Case #{}: {}'.format(t+1,lword))
