ad76eea5ef207bf591824f69e5f48dc039f92dde
Praveen Kumar Pendyala <praveendath92@gmail.com>
2016-04-28 01:20:52 +0200

    2008 fixes

--- 2009/qualification/c.py
+++ 2009/qualification/c.py
@@ -3,16 +3,16 @@ import re
 
 W = 'welcome to code jam'
 
-def findcounts(S, cpos):
-    count = 0
-    for offset in re.finditer(W[cpos],S):
-        if cpos < len(W)-1:
-            count += findcounts(S[offset.start():], cpos+1)
-        else:
-            count += 1
-    return count
+def DP(S, cpos):
+    if cpos == 0:
+        yield len(re.findall(W[cpos], S))
+    elif S.endswith(W[cpos]):
+        yield DP(S[:-1],cpos-1) + DP(S[:-1], cpos)
+    else:
+        yield DP(S[:-1],cpos)
+
 
 T = readInt()
 for t in range(T):
     S = readLineRaw()
-    writeLine('Case #{}: {:04}'.format(t+1, findcounts(S,0)))
+    writeLine('Case #{}: {:04}'.format(t+1, DP(S,len(W)-1)))
